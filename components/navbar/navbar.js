7585fe09e0150bfae30d6c9095d2a7ef0012f7ca
Pav0l <pavol.karas@gmail.com>
2019-02-05 23:51:44 +0100

    Delete comments

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,16 +1,17 @@
 class Dropdown {
-    constructor(element) {
-      this.element = element;
+  constructor(element) {
+    this.element = element;
 
-      this.button = this.element.querySelector('.dropdown-btn');      
-      this.content = this.element.querySelector('.dropdown-content');
-      
-      this.button.addEventListener('click', () => this.toggleContent());
-    }
-  
-    toggleContent() {
-      this.content.classList.toggle('dropdown-hidden');
-    }
+    this.button = this.element.querySelector('.dropdown-btn');      
+    this.content = this.element.querySelector('.dropdown-content');
+    
+    this.button.addEventListener('click', () => this.toggleContent());
   }
 
-  let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+  toggleContent() {
+    this.content.classList.toggle('dropdown-hidden');
+  }
+}
+
+let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+
