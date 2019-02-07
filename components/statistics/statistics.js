a5297fa3eecdf6e098a84b3613b3f79cbaa66bf2
Pav0l <pavol.karas@gmail.com>
2019-02-07 22:28:55 +0100

    Fix indentation, edit comments

--- components/statistics/statistics.js
+++ components/statistics/statistics.js
@@ -1,49 +1,30 @@
 class Stats {
     constructor(stat) {
-        // grab stats item
         this.stat = stat;
-        // grab its text value
         this.text = this.stat.textContent;
-        // grab its datatype
         this.dataValue = this.stat.dataset.value;        
         // define the starting point for counter
         let counter = 0;
 
-        // add CLICK eventlistener with callback that will set setInterval on method
-        // setInterval will repeadetly calls this.startCounter function counter >= this.dataValue
-        /*
-        statsElement.addEventListener('click', () => {        
-            let startInt = setInterval(() => {
-                counter += 1;
-                this.startCounter(counter);
-                if (counter >= parseInt(this.dataValue)) {
-                    clearInterval(startInt);
-                }
-            }, 0.01) 
-        });
-        */
-
         // This event listener can be performance heavy.
-        // if (window.scrollY / bodyHeight > 0.50) {}
-            window.addEventListener("scroll", (event) => {
-                let scroll = window.scrollY;
-                // Calculate the scroll bar position divided by window height. 
-                // 0.57 is ratio where the elements sits on desktop screen
-                if (scroll / bodyHeight > 0.57 && parseInt(this.stat.textContent) <= this.text) {
-                    // setInterval will repeadetly calls this.startCounter 
-                    let startInt = setInterval(() => {
-                        counter += 1;
-                        this.startCounter(counter);
-                        // setInterval stops if the counter reaches dataset constant
-                        if (counter >= parseInt(this.dataValue)) {
-                            clearInterval(startInt);
-                        }
-                    }, 0.01)
-                }
-            });
-
-
-
+        window.addEventListener("scroll", (event) => {
+            // get vertical scroll bar position
+            let scroll = window.scrollY;
+            // Calculate the scroll bar position divided by window height. 
+            // 0.57 is ratio where the elements sits on desktop screen
+            // and if current text content is smaller or equal to dataset const
+            if (scroll / bodyHeight > 0.57 && parseInt(this.stat.textContent) <= this.text) {
+                // setInterval will repeadetly calls this.startCounter 
+                let startInt = setInterval(() => {
+                    counter += 1;
+                    this.startCounter(counter);
+                    // setInterval stops if the counter reaches dataset constant
+                    if (counter >= parseInt(this.dataValue)) {
+                        clearInterval(startInt);
+                    }
+                }, 0.01)
+            }
+        });
     }
     
     // pass in the counter value as a text value to be displayed on page
